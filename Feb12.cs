6dc494e42e3aa3cb35c83901d483a0b9cf5d4cfd
Mantajbhatti <mantajb082@stemcollegiate.ca>
2025-02-12 09:28:23 -0700

    Create Feb12.cs

---
+++ Feb12.cs
@@ -0,0 +1,11 @@
+//Feb12.cs 
+// i liked how i learned new codes ands learned about strings 
+//i didnt like how i didnt learn about the first modual but will do that at home so its okay
+//Console.WriteLine('b');
+Console.WriteLine ("Hello World!");
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);

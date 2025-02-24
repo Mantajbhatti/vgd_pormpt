958134f2ece584b03f76a636417392064b224f45
Mantajbhatti <mantajb082@stemcollegiate.ca>
2025-02-24 14:04:56 -0700

    Update Feb24.cs

--- Feb24.cs
+++ Feb24.cs
@@ -1,2 +1,13 @@
 var message = "Hello world!";
 i leanred that the code doesnt work and i learned about the code VAR
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");

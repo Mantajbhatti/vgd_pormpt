28bf0833efc71050e73903c276201323de207693
Mantajbhatti <mantajb082@stemcollegiate.ca>
2025-03-05 09:02:39 -0700

    Create March05.cs

---
+++ March05.cs
@@ -0,0 +1,9 @@
+// March 05
+// today i learned about litreal value and the diffrent types 
+Console.WriteLine('H');
+Console.WriteLine(12567);
+Console.WriteLine(0.67);
+Console.WriteLine(487.5);
+Console.WriteLine(12.993316m);
+Console.WriteLine(true);
+Console.WriteLine(false);

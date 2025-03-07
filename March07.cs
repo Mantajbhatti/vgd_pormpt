32c69c4f653d6e9d323050e3aa282fb6108882f1
Mantajbhatti <mantajb082@stemcollegiate.ca>
2025-03-07 10:20:18 -0700

    Create March07.cs

---
+++ March07.cs
@@ -0,0 +1,15 @@
+string firstName;
+firstName= "Taj";
+Console.WriteLine("Taj");
+string name = "Bobby";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
+Today I learned how to write more code 

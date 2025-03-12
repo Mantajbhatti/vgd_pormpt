b33cdf47d0f10677eee040eeba6f6e21935a9665
Mantajbhatti <mantajb082@stemcollegiate.ca>
2025-03-12 08:49:04 -0600

    Create March12.cs

---
+++ March12.cs
@@ -0,0 +1,9 @@
+string firstName = "Mantaj";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}!");
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+i learned how to simplify codes even more 
